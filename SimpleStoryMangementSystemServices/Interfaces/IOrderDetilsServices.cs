44028f5da60974c66da1cc9d2f715e6e7c820c36
Kamalmohamed_Br <158767610+KamalMoh7amed@users.noreply.github.com>
2026-03-05 23:49:03 +0200

    Add files via upload

---
+++ SimpleStoryMangementSystemServices/Interfaces/IOrderDetilsServices.cs
@@ -0,0 +1,17 @@
+﻿using SimpleStoryMangementSystem.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SimpleStoryMangementSystem.Services.Interfaces;
+
+public interface IOrderDetilsServices
+{
+        void AddOrderDetails(OrderDetails orderDetails);
+        void DeleteOrderDetails(int Id);
+        void UpdateOrderDetails(OrderDetails orderDetails);
+    
+        List<OrderDetails> GetAllOrderDetails();
+
+        (bool, OrderDetails) SearchOrderDetailsById(int id);
+}
---
+++ SimpleStoryMangementSystemServices/Interfaces/IOrderServices.cs
@@ -0,0 +1,18 @@
+﻿using SimpleStoryMangementSystem.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SimpleStoryMangementSystem.Services.Interfaces;
+
+public interface IOrderServices
+{
+    void AddOrder(Order order);
+    void DeleteOrder(int Id);
+    void UpdateOrder(Order order);
+
+    List<Order> GetAllOrders();
+
+    bool SearchOrderById(int id);
+
+}
