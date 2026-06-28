6b014aa4ff713bae807c5e9f6b80c720e486611a
Kamalmohamed_Br <158767610+KamalMoh7amed@users.noreply.github.com>
2026-06-29 00:15:09 +0300

    Final_Docs_ITC_2026

--- Etmen_PL/DependencyInjection.cs
+++ Etmen_PL/DependencyInjection.cs
@@ -1,37 +1,70 @@
-﻿using Etmen_BLL.Helpers;
-using Etmen_BLL.Mapping;
-using Etmen_BLL.Repositories.IServices;
-using Etmen_BLL.Repositories.Services;
-using Etmen_DAL.DbContext;
-using Etmen_DAL.Repositories.Implementations;
-using Etmen_DAL.Repositories.Interfaces;
-using Etmen_Domain.Entities;
-using Mapster;
-using Microsoft.AspNetCore.Http.Features;
-using Microsoft.AspNetCore.Identity;
-using Microsoft.EntityFrameworkCore;
-
-namespace Etmen_PL
-{
-    public static class DependencyInjection
-    {
-        public static IServiceCollection AddApplicationServices(this IServiceCollection services, IConfiguration configuration, IWebHostEnvironment environment)
-        {
-            // 1. DATABASE CONTEXT
-            services.AddDbContext<EtmenDbContext>(options =>
-                options.UseSqlServer(
-                    configuration.GetConnectionString("DefaultConnection"),
-                    sql =>
-                    {
-                        sql.MigrationsAssembly(typeof(EtmenDbContext).Assembly.FullName);
-                        sql.CommandTimeout(60);
-                        sql.EnableRetryOnFailure(3);
-                    }
-                )
-            );
-            // 2. IDENTITY
+﻿using Etmen_BLL.Helpers;
+
+using Etmen_BLL.Mapping;
+
+using Etmen_BLL.Repositories.IServices;
+
+using Etmen_BLL.Repositories.Services;
+
+using Etmen_DAL.DbContext;
+
+using Etmen_DAL.Repositories.Implementations;
+
+using Etmen_DAL.Repositories.Interfaces;
+
+using Etmen_Domain.Entities;
+
+using Mapster;
+
+using Microsoft.AspNetCore.Http.Features;
+
+using Microsoft.AspNetCore.Identity;
+
+using Microsoft.EntityFrameworkCore;
+
+
+
+namespace Etmen_PL
+
+{
+
+    public static class DependencyInjection
+
+    {
+
+        public static IServiceCollection AddApplicationServices(this IServiceCollection services, IConfiguration configuration, IWebHostEnvironment environment)
+
+        {
+
+            // 1. DATABASE CONTEXT
+
+            services.AddDbContext<EtmenDbContext>(options =>
+
+                options.UseSqlServer(
+
+                    configuration.GetConnectionString("DefaultConnection"),
+
+                    sql =>
+
+                    {
+
+                        sql.MigrationsAssembly(typeof(EtmenDbContext).Assembly.FullName);
+
+                        sql.CommandTimeout(60);
+
+                        sql.EnableRetryOnFailure(3);
+
+                    }
+
+                )
+
+            );
+
+            // 2. IDENTITY
+
             services.AddIdentity<ApplicationUser, IdentityRole>(options =>
-            {
+            {
+
                 options.Password.RequireDigit = true;
                 options.Password.RequireLowercase = true;
                 options.Password.RequireUppercase = false;
@@ -39,106 +72,177 @@ namespace Etmen_PL
                 options.Password.RequiredLength = 6;
                 options.User.RequireUniqueEmail = true;
                 options.SignIn.RequireConfirmedEmail = true;
-                options.Lockout.DefaultLockoutTimeSpan = TimeSpan.FromMinutes(
-                    configuration.GetValue<int>("Security:LockoutDurationInMinutes", 15));
-                options.Lockout.MaxFailedAccessAttempts = configuration.GetValue<int>("Security:MaxLoginAttempts", 5);
-                options.Lockout.AllowedForNewUsers = true;
+                options.Lockout.DefaultLockoutTimeSpan = TimeSpan.FromMinutes(
+
+                    configuration.GetValue<int>("Security:LockoutDurationInMinutes", 15));
+
+                options.Lockout.MaxFailedAccessAttempts = configuration.GetValue<int>("Security:MaxLoginAttempts", 5);
+
+                options.Lockout.AllowedForNewUsers = true;
+
             })
             .AddEntityFrameworkStores<EtmenDbContext>()
-            .AddDefaultTokenProviders();
-            services.ConfigureApplicationCookie(options =>
-            {
-                options.LoginPath = "/Account/Login";
-                options.LogoutPath = "/Account/Logout";
-                options.AccessDeniedPath = "/Account/AccessDenied";
-
-                options.Cookie.Name = "EtmenAuthCookie";
-                options.Cookie.HttpOnly = true;
-                options.Cookie.SecurePolicy = CookieSecurePolicy.SameAsRequest;
-                options.Cookie.SameSite = SameSiteMode.Lax;
-
-                options.ExpireTimeSpan = TimeSpan.FromDays(
-                    configuration.GetValue<int>("Security:CookieExpirationDays", 7));
-                options.SlidingExpiration = true;
-            });
+            .AddDefaultTokenProviders();
+
+            services.ConfigureApplicationCookie(options =>
+
+            {
+
+                options.LoginPath = "/Account/Login";
+
+                options.LogoutPath = "/Account/Logout";
+
+                options.AccessDeniedPath = "/Account/AccessDenied";
+
+
+
+                options.Cookie.Name = "EtmenAuthCookie";
+
+                options.Cookie.HttpOnly = true;
+
+                options.Cookie.SecurePolicy = CookieSecurePolicy.SameAsRequest;
+
+                options.Cookie.SameSite = SameSiteMode.Lax;
+
+
+
+                options.ExpireTimeSpan = TimeSpan.FromDays(
+
+                    configuration.GetValue<int>("Security:CookieExpirationDays", 7));
+
+                options.SlidingExpiration = true;
+
+            });
+
 
             // Unit of Work
-            services.AddScoped<IUnitOfWork, UnitOfWork>();
-            //  HTTP CONTEXT ACCESSOR 
-            services.AddHttpContextAccessor();
+            services.AddScoped<IUnitOfWork, UnitOfWork>();
+
+            //  HTTP CONTEXT ACCESSOR 
+
+            services.AddHttpContextAccessor();
+
             //  BUSINESS 
-            services.AddScoped<IEmailService, EmailService>();
-            services.AddScoped<IPdfReportService, PdfReportService>();
-
-            //  background task queue and hosted service
-            services.AddSingleton<IBackgroundTaskQueue>(new BackgroundTaskQueue(100));
-            services.AddHostedService<QueuedHostedService>();
-            // Appointment reminder background service (checks every 30 minutes)
-            services.AddHostedService<AppointmentReminderHostedService>();
+            services.AddScoped<IEmailService, EmailService>();
 
-            // Auth
-            services.AddScoped<IAuthService, AuthService>();
-            // Patient
-            services.AddScoped<IPatientService, PatientService>();
-            services.AddScoped<IMedicalRecordService, MedicalRecordService>();
+            services.AddScoped<IPdfReportService, PdfReportService>();
 
-            // Doctor
-            services.AddScoped<IDoctorService, DoctorService>();
-            // Appointments
-            services.AddScoped<IAppointmentService, AppointmentService>();
-            services.AddScoped<INearbyService, NearbyService>();
-            // Health Monitoring
-            services.AddScoped<IRiskService, RiskService>();
-            services.AddScoped<ILabService, LabService>();
-            services.AddScoped<IAlertService, AlertService>();
-            // Communications
-            services.AddScoped<INotificationService, NotificationService>();
-            services.AddScoped<IChatService, ChatService>();
-            services.AddScoped<IReviewService, ReviewService>();
-            services.Configure<ChatbotApiOptions>(
-                    configuration.GetSection("ChatbotApi"));
 
-            services.AddHttpClient<IChatbotService, ChatbotService>()
-                .ConfigureHttpClient(client =>
-                {
-                    client.Timeout = TimeSpan.FromSeconds(30);
-                });
-            // Family
-            services.AddScoped<IFamilyService, FamilyService>();
+
+            //  background task queue and hosted service
+
+            services.AddSingleton<IBackgroundTaskQueue>(new BackgroundTaskQueue(100));
+
+            services.AddHostedService<QueuedHostedService>();
+
+            // Appointment reminder background service (checks every 30 minutes)
+
+            services.AddHostedService<AppointmentReminderHostedService>();
+
+
+            // Auth
+
+            services.AddScoped<IAuthService, AuthService>();
+
+
+            // Patient
+
+            services.AddScoped<IPatientService, PatientService>();
+
+            services.AddScoped<IMedicalRecordService, MedicalRecordService>();
+
+
+            // Doctor
+
+            services.AddScoped<IDoctorService, DoctorService>();
+
+            // Appointments
+
+            services.AddScoped<IAppointmentService, AppointmentService>();
+
+            services.AddScoped<INearbyService, NearbyService>();
+
+            // Health Monitoring
+
+            services.AddScoped<IRiskService, RiskService>();
+
+            services.AddScoped<ILabService, LabService>();
+
+            services.AddScoped<IAlertService, AlertService>();
+
+
+            // Communications
+
+            services.AddScoped<INotificationService, NotificationService>();
+
+            services.AddScoped<IChatService, ChatService>();
+
+            services.AddScoped<IReviewService, ReviewService>();
+
+            services.Configure<ChatbotApiOptions>(
+
+                    configuration.GetSection("ChatbotApi"));
+
+
+            services.AddHttpClient<IChatbotService, ChatbotService>()
+
+                .ConfigureHttpClient(client =>
+
+                {
+
+                    client.Timeout = TimeSpan.FromSeconds(30);
+
+                });
+
+            // Family
+
+            services.AddScoped<IFamilyService, FamilyService>();
+
             // Emergency
             services.AddScoped<IEmergencyService, EmergencyService>();
             services.AddScoped<IHospitalStaffService, HospitalStaffService>();
             services.AddScoped<ICriticalCareEscalationService, CriticalCareEscalationService>();
-            services.AddScoped<ICriticalIntelligenceService, CriticalIntelligenceService>();
+            services.AddScoped<ICriticalIntelligenceService, CriticalIntelligenceService>();
+
             // Crisis
             services.AddScoped<ICrisisService, CrisisService>();
-            services.AddScoped<ICrisisRiskEngineService, CrisisRiskEngineService>();
+            services.AddScoped<ICrisisRiskEngineService, CrisisRiskEngineService>();
+
             // Admin
-            services.AddScoped<IAdminService, AdminService>();
-
+            services.AddScoped<IAdminService, AdminService>();
+
+
+
             // 5. MAPSTER
             var mapsterConfig = TypeAdapterConfig.GlobalSettings;
             mapsterConfig.Scan(typeof(BLLMappingProfile).Assembly);   
             mapsterConfig.Compile();                                    
-            services.AddSingleton(mapsterConfig);
-
+            services.AddSingleton(mapsterConfig);
+
+
+
             // 6. MVC
             services.AddControllersWithViews(options =>
             {
                 options.Filters.Add<Etmen_PL.Filters.DoctorOnboardingFilter>();
                 options.Filters.Add<Etmen_PL.Filters.PatientProfileFilter>();
                 options.Filters.Add<Etmen_PL.Filters.MaintenanceFilter>();
-            })
+            })
+
                 .AddJsonOptions(options =>
                 {
                     options.JsonSerializerOptions.ReferenceHandler =
                         System.Text.Json.Serialization.ReferenceHandler.IgnoreCycles;
                    options.JsonSerializerOptions.WriteIndented = environment.IsDevelopment();
-                });
-
+                });
+
+
+
             services.AddRazorPages();
-            services.AddSignalR();
-            // 7. SESSION
+            services.AddSignalR();
+
+            // 7. SESSION
+
             services.AddDistributedMemoryCache();
             services.AddSession(options =>
             {
@@ -148,38 +252,72 @@ namespace Etmen_PL
                 options.Cookie.IsEssential = true;
                 options.Cookie.SecurePolicy = CookieSecurePolicy.Always;
                 options.Cookie.Name = "EtmenSession";
-            });
+            });
+
             // 8. CACHING
-            services.AddMemoryCache();      
+            services.AddMemoryCache();
+      
+
             // 9. HTTP CLIENT (for external APIs: OCR, AI, etc.)
-            services.AddHttpClient();
-            // 10. CORS
-            services.AddCors(options =>
-            {
-                options.AddPolicy("AllowSpecificOrigin", policy =>
-                    policy
-                        .WithOrigins("https://localhost:5001", "https://localhost:7001")
-                        .AllowAnyHeader()
-                        .AllowAnyMethod()
-                        .AllowCredentials());
-            });
-
-            // 11. ANTI-FORGERY
-            services.AddAntiforgery(options =>
-            {
-                options.HeaderName = "X-CSRF-TOKEN";
-                options.Cookie.Name = "EtmenAntiForgery";
-                options.Cookie.SecurePolicy = CookieSecurePolicy.SameAsRequest;
-            });
-
-            // 12. FILE UPLOAD LIMIT (10 MB)
-            services.Configure<FormOptions>(options =>
-            {
-                int maxMb = configuration.GetValue<int>("FileUpload:MaxFileSizeInMB", 10);
-                options.MultipartBodyLengthLimit = maxMb * 1024 * 1024;
-            });
-
-            return services;
-        }
-    }
-}
+            services.AddHttpClient();
+
+            // 10. CORS
+
+            services.AddCors(options =>
+
+            {
+
+                options.AddPolicy("AllowSpecificOrigin", policy =>
+
+                    policy
+
+                        .WithOrigins("https://localhost:5001", "https://localhost:7001")
+
+                        .AllowAnyHeader()
+
+                        .AllowAnyMethod()
+
+                        .AllowCredentials());
+
+            });
+
+
+
+            // 11. ANTI-FORGERY
+
+            services.AddAntiforgery(options =>
+
+            {
+
+                options.HeaderName = "X-CSRF-TOKEN";
+
+                options.Cookie.Name = "EtmenAntiForgery";
+
+                options.Cookie.SecurePolicy = CookieSecurePolicy.SameAsRequest;
+
+            });
+
+
+
+            // 12. FILE UPLOAD LIMIT (10 MB)
+
+            services.Configure<FormOptions>(options =>
+
+            {
+
+                int maxMb = configuration.GetValue<int>("FileUpload:MaxFileSizeInMB", 10);
+
+                options.MultipartBodyLengthLimit = maxMb * 1024 * 1024;
+
+            });
+
+
+
+            return services;
+
+        }
+
+    }
+
+}
+
